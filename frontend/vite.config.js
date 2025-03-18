aa5f2f47577f707a697e8c665fbb89e7a44ac8ac
suryanshu <suryanshutiwari00@gmail.com>
2025-03-18 23:56:44 +0530

    deployment changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000/api/v1'
+      '/api': 'https://video-sharehub-1.onrender.com'
     }
   }
 })
